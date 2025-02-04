da1b7495569056c045cc1f65ba53a2906157d911
Callum <callum3gill@gmail.com>
2025-02-04 20:28:24 +0000

    Set device to auto to run on GPU if possible

--- RLTraining.py
+++ RLTraining.py
@@ -1,19 +1,22 @@
 import pandas as pd
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+
 from RLModel import SchedulingEnv
 
 
 def main():
     # Load datasets
+    print("Loading datasets...")
     teachers = pd.read_csv("teachers.csv")
     students = pd.read_csv("students.csv")
     rooms = pd.read_csv("rooms.csv")
 
+    print("Setting up model...")
     env = SchedulingEnv(teachers, students, rooms)
     vec_env = make_vec_env(lambda: env, n_envs=1)
 
-    model = PPO("MultiInputPolicy", vec_env, verbose=1)
+    model = PPO("MultiInputPolicy", vec_env, verbose=1, device="auto")
 
     # Adjust time-steps for better results but longer training time
     model.learn(total_timesteps=10000)
